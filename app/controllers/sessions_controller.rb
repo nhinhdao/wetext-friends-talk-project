e808a9184e061eb80f7228a71eb5800a89d934a4
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-11-09 19:43:07 -0500

    add sessions#welcome

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,20 +1,41 @@
 class SessionsController < ApplicationController
   def welcome
+    if !logged_in?
+      redirect_to 'login'
+    else
+      render :welcome
+    end
   end
 
   def new
+    @user = User.new
   end
 
   def create
-    @user = User.find_or_create_by(uid: auth['uid']) do |u|
-      u.username = auth['info']['name']
-      u.email = auth['info']['email']
-      u.image = auth['info']['image']
+    if params[:email].present? && params[:password].present? #user logged in through form provided in the home page
+      user = User.find_by(:email => params[:email])
+      if user && user.authenticate(params[:password])
+        user.image = Faker::Avatar.image
+        session[:current_user_id] = @user.id
+        redirect_to '/'
+      else
+        flash.now[:notice] = "Could not find that person, sorry!"
+        render :new
+      end
+    elsif request.env['omniauth.auth'].present? #user logged in through facebook
+      @user = User.find_or_create_by(uid: auth['uid']) do |u|
+        u.username = auth['info']['name']
+        u.email = auth['info']['email']
+        u.image = auth['info']['image']
+      end
+      session[:current_user_id] = @user.id
+      redirect_to '/'
     end
+  end
 
-    session[:user_id] = @user.id
-
-    render 'sessions/welcome'
+  def destroy
+    session.destroy
+    redirect_to '/'
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -8,17 +8,11 @@ Rails.application.routes.draw do
 
   resources :connections
 
-  #
-  # get 'login'
-  # post 'login'
-  # get 'signup'
-  # post 'signup'
+  
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  get 'signup' => 'users/new'
+  post 'signup' => 'users/create'
   get '/auth/facebook/callback' => 'sessions#create'
-  # get 'logout'
-  # post 'logout'
-  # get 'posts/new'
-  # get 'posts/destroy'
-  # get 'messages/new'
-  # get 'users/new' => 'users#new'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post 'logout' => 'sessions#destroy'
 end
