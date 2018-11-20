029796065bb801766ee6d7e6aadfb52560387d93
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-11-19 23:43:23 -0500

    Updated user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
-  validates :password, presence: true, on: :create
+  validates :password, presence: true
   validates :password_confirmation, presence: true
 
   has_secure_password
